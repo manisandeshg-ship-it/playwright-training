c5cfaf24f34d86cf68953bb934b09de3fd25109b
Sandesh Gandra <sandeshgandra16@gmail.com>
2026-05-16 14:33:10 -0400

    updated tags 3

--- tests/swaglabs.spec.ts
+++ tests/swaglabs.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('saucedemo - login with standard_user', { tag: '@smoke' }, async ({ page }) => {
+test('saucedemo - login with standard_user', { tag: ['@smoke'] }, async ({ page }) => {
   // navigate to saucedemo
   await page.goto('https://www.saucedemo.com/');
 
